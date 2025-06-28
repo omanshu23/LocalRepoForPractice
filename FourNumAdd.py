0b9dbf934f569b6d69307feba472d11cfe95ee91
omanshu23 <writemeom@gmail.com>
2025-06-28 16:58:26 +0530

    Add Initial Files

---
+++ FourNumAdd.py
@@ -0,0 +1,13 @@
+# Add four numbers in Python
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+num4 = float(input("Enter fourth number: "))
+
+# Add the numbers
+sum = num1 + num2 + num3 + num4
+
+# Display the result
+print("The sum is:", sum)
---
+++ ThreeNumAdd.py
@@ -0,0 +1,12 @@
+# Add three numbers in Python
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+# Add the numbers
+sum = num1 + num2 + num3
+
+# Display the result
+print("The sum is:", sum)

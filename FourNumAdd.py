8b06cfd223e0413d205a41a43e5837c68bc47e81
omanshu23 <writemeom@gmail.com>
2025-06-28 17:31:01 +0530

    Add Space in 8th line

--- FourNumAdd.py
+++ FourNumAdd.py
@@ -5,7 +5,7 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 num3 = float(input("Enter third number: "))
 num4 = float(input("Enter fourth number: "))
-num5 = float(input("Enter fifth number: "))
+num5 = float(input("Enter fifth number:   "))
 # Add the numbers
 sum = num1 + num2 + num3 + num4 + num5
 
